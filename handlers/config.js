97a4526f113e7d5b7cbc11de4bf7df3c783907bf
shyamg91 <shyamg7777@gmail.com>
2017-07-16 23:12:46 +0530

    Facebook redirect fix

--- handlers/config.js
+++ handlers/config.js
@@ -3,6 +3,7 @@ module.exports = {
   'facebookAuth': {
     'clientID': '1264889470303519', // your App ID
     'clientSecret': '2c328e093fd45cd0f61cac4c0d1406b3', // your App Secret
-    'callbackURL': 'http://localhost:7777/auth/facebook/callback'
+    'callbackURL': 'https://sleepy-woodland-19821.herokuapp.com/auth/facebook/callback'
+    // 'http://localhost:7777/auth/facebook/callback'
   }
 }
